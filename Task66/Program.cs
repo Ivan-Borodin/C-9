dfb76bf883020d7a24bb46e6ac22decc4721ea88
Ivan <ivan241578@gmail.com>
2022-06-12 21:03:24 +0300

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void SumNumber(int numM, int numN, int sum)
+{
+    if (numM == numN)
+    {
+        sum = sum + numM;
+        Console.WriteLine(sum);
+        return;
+    }
+    if (numN > numM)
+    {
+        sum = sum + numM;
+        SumNumber(numM + 1, numN, sum);
+    }
+    if (numM > numN)
+    {
+        sum = sum + numM;
+        SumNumber(numM - 1, numN, sum);
+    }
+}
+SumNumber(m, n, 0);
