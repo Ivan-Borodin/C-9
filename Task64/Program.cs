2f15e37b17d6b0effc2cb916173c52e42e749350
Ivan <ivan241578@gmail.com>
2022-06-12 21:02:39 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+void NNumber (int x, int y)
+{
+    if (x>y) return;
+    NNumber (x+1, y);
+    Console.Write($"{x} ");
+}
+NNumber(m,n);
